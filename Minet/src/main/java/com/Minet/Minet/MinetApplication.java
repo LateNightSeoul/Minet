89f102ac247335ae98bd526c1714f3167ac33fd9
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-05-01 22:22:42 +0900

    init

---
+++ Minet/src/main/java/com/Minet/Minet/MinetApplication.java
@@ -0,0 +1,13 @@
+package com.Minet.Minet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinetApplication.class, args);
+	}
+
+}
---
+++ Minet/src/test/java/com/Minet/Minet/MinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Minet.Minet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
