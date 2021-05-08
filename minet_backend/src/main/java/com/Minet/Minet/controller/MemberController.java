078a440fb6cc8e0b0665d2b8d3b6e542f0b14990
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-05-09 07:21:34 +0900

    feat : 로그인 기능 구현

--- minet_backend/src/main/java/com/Minet/Minet/controller/MemberController.java
+++ minet_backend/src/main/java/com/Minet/Minet/controller/MemberController.java
@@ -2,16 +2,27 @@ package com.Minet.Minet.controller;
 
 import com.Minet.Minet.domain.member.Artist;
 import com.Minet.Minet.domain.member.Member;
+import com.Minet.Minet.dto.TokenDto;
 import com.Minet.Minet.dto.member.JoinArtistDto;
 import com.Minet.Minet.dto.member.JoinMemberDto;
+import com.Minet.Minet.dto.member.LoginDto;
+import com.Minet.Minet.jwt.JwtFilter;
+import com.Minet.Minet.jwt.TokenProvider;
 import com.Minet.Minet.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.net.http.HttpResponse;
 import java.util.Arrays;
 import java.util.List;
@@ -22,6 +33,12 @@ import java.util.Map;
 public class MemberController {
 
     @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    @Autowired
     private MemberService memberService;
 
     @PostMapping("/join/member")
@@ -29,6 +46,24 @@ public class MemberController {
             @RequestBody JoinMemberDto joinMemberDto
             ) {
         return ResponseEntity.ok(memberService.joinMember(joinMemberDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginDto loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUserid(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+
     }
 
 }
