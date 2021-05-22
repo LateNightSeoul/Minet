c30bb6871dd5a58b7d28f38716f53a7a6f086000
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-05-22 23:04:51 +0900

    TEST : 서브쿼리 통합 테스트

--- minet_backend/src/test/java/com/Minet/Minet/service/CreateDailyChartServiceTest.java
+++ minet_backend/src/test/java/com/Minet/Minet/service/CreateDailyChartServiceTest.java
@@ -56,6 +56,7 @@ public class CreateDailyChartServiceTest {
     public void testWhole() throws Exception {
         createChartInsertData();
         createChart();
+        testSubQuery();
     }
 
     @Test
@@ -111,15 +112,22 @@ public class CreateDailyChartServiceTest {
 
             dailyVisitedRepository.save(dailyVisited2);
 
-            for (int j = 0; j < random.nextInt(1000); j++) {
+            int upperInt = random.nextInt(100);
+
+            for (int j = 0; j < upperInt + random.nextInt(100); j++) {
                 SongLikeId songLikeId1 = new SongLikeId(albumChildId, LocalDateTime.now().minusSeconds(random.nextInt(10000)));
-                SongLikeId songLikeId2 = new SongLikeId(albumChildId, LocalDateTime.now().minusDays(1).minusSeconds(random.nextInt(10000)));
 
                 SongLike songLike = new SongLike();
                 songLike.setSong(song);
                 songLike.setSongLikeId(songLikeId1);
 
                 songLikeRepository.save(songLike);
+
+                em.flush();
+            }
+
+            for (int j = 0; j < upperInt; j++) {
+                SongLikeId songLikeId2 = new SongLikeId(albumChildId, LocalDateTime.now().minusDays(1).minusSeconds(random.nextInt(10000)));
 
                 SongLike songLike2 = new SongLike();
                 songLike2.setSong(song);
@@ -161,6 +169,37 @@ public class CreateDailyChartServiceTest {
             for(int i = 0; i < s.length; i++) {
                 System.out.println(s[i]);
             }
+        }
+    }
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testSubQuery() {
+        List<Object[]> songLike = em.createNativeQuery("SELECT S1.SONG_URL, (S1.CNT - S2.CNT) AS LIKE_INC, S1.CNT AS CNT1, S2.CNT AS CNT2 " +
+                "FROM (SELECT SL.SONG_URL, COUNT(SL.SONG_URL) AS CNT FROM SONG_LIKE SL WHERE FORMATDATETIME(SL.CREATE_DATE, 'yyyy-MM-dd') = FORMATDATETIME(:localdate1, 'yyyy-MM-dd') GROUP BY SL.SONG_URL) AS S1," +
+                "(SELECT SL.SONG_URL, COUNT(SL.SONG_URL) AS CNT FROM SONG_LIKE SL WHERE FORMATDATETIME(SL.CREATE_DATE, 'yyyy-MM-dd') = FORMATDATETIME(:localdate2, 'yyyy-MM-dd') GROUP BY SL.SONG_URL) AS S2 " +
+                "WHERE S1.SONG_URL = S2.SONG_URL ORDER BY S1.SONG_URL ASC")
+                .setParameter("localdate1", LocalDateTime.now())
+                .setParameter("localdate2", LocalDateTime.now().minusDays(1))
+                .getResultList();
+
+        for(Object[] s : songLike) {
+            System.out.println("SONG_URL : " + s[0] + "  LIKE_INC : " + s[1] + " REST : " + s[2] + " " + s[3]);
+        }
+
+        List<Object[]> visited = em.createNativeQuery("SELECT V1.SONG_URL, (V1.COUNT - V2.COUNT) AS CNT " +
+                "FROM (SELECT DV.SONG_URL, DV.COUNT FROM DAILY_VISITED AS DV WHERE FORMATDATETIME(DV.CREATE_DATE, 'yyyy-MM-dd') = FORMATDATETIME(:localdate1, 'yyyy-MM-dd') GROUP BY DV.SONG_URL) AS V1," +
+                "(SELECT DV.SONG_URL, DV.COUNT FROM DAILY_VISITED AS DV WHERE FORMATDATETIME(DV.CREATE_DATE, 'yyyy-MM-dd') = FORMATDATETIME(:localdate2, 'yyyy-MM-dd') GROUP BY DV.SONG_URL) AS V2 " +
+                "WHERE V1.SONG_URL = V2.SONG_URL ORDER BY V1.SONG_URL ASC")
+                .setParameter("localdate1", LocalDateTime.now())
+                .setParameter("localdate2", LocalDateTime.now().minusDays(1))
+                .getResultList();
+
+        System.out.println("***********************************************************");
+
+        for(Object[] a : visited) {
+            System.out.println("Song_URL : " + a[0] + " CNT : " + a[1]);
         }
     }
 }
