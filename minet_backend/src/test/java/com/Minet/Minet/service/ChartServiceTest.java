b08d29b4d3b270926818682a40cca3bce70a44a2
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-05-23 20:10:05 +0900

    test : rising chart 불러오기 기능 테스트

--- minet_backend/src/test/java/com/Minet/Minet/service/ChartServiceTest.java
+++ minet_backend/src/test/java/com/Minet/Minet/service/ChartServiceTest.java
@@ -1,4 +1,32 @@
 package com.Minet.Minet.service;
 
+import com.Minet.Minet.domain.music.ChartSong;
+import com.Minet.Minet.repository.ChartRepository;
+import com.Minet.Minet.repository.ChartSongRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ChartServiceTest {
+
+    @Autowired
+    ChartService chartService;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void findChart() throws Exception {
+        List<ChartSong> risingChart = chartService.getRisingChart();
+        for (ChartSong song : risingChart) {
+            System.out.println(song.getSongName() + " " + song.getSongChildId() + " " + song.getGenre());
+        }
+    }
 }
