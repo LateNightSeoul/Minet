c093f1acc13c1680dfa460fd7c77ee953a45fec5
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-05-24 16:30:51 +0900

    refactor : Rising 차트 엔티티 수정

--- minet_backend/src/main/java/com/Minet/Minet/service/ChartService.java
+++ minet_backend/src/main/java/com/Minet/Minet/service/ChartService.java
@@ -1,18 +1,15 @@
 package com.Minet.Minet.service;
 
-import com.Minet.Minet.controller.response.RisingChartResponse;
 import com.Minet.Minet.domain.enumTypes.ChartType;
 import com.Minet.Minet.domain.music.Chart;
 import com.Minet.Minet.domain.music.ChartSong;
 import com.Minet.Minet.repository.ChartRepository;
 import com.Minet.Minet.repository.ChartSongRepository;
-import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,7 +22,7 @@ public class ChartService {
     ChartSongRepository chartSongRepository;
 
     @Transactional
-    public List<RisingChartResponse> getRisingChart() throws IllegalAccessException {
+    public List<ChartSong> getRisingChart() throws IllegalAccessException {
         Chart risingChart = null;
         List<Chart> findChart = chartRepository.findByChartDate(LocalDate.now());
         for(Chart chart : findChart) {
@@ -36,11 +33,6 @@ public class ChartService {
         if(risingChart == null) {
             throw new IllegalAccessException("차트가 존재하지 않습니다.");
         }
-        List<RisingChartResponse> risingChartResponses = new ArrayList<>();
-        List<ChartSong> chartSongs = chartSongRepository.findChartSongsByChart(risingChart);
-
-        for (ChartSong chartSong : chartSongs) {
-            
-        }
+        return chartSongRepository.findChartSongsByChart(risingChart);
     }
 }
