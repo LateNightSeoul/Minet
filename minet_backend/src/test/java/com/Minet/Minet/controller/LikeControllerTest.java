eac7d348732a6ad2e528d17b7e3afb0b0dede50c
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-05-27 16:23:04 +0900

    test : 추상 컨트롤러 테스트 구현 및 Like controller 테스트 구현

---
+++ minet_backend/src/test/java/com/Minet/Minet/controller/LikeControllerTest.java
@@ -0,0 +1,29 @@
+package com.Minet.Minet.controller;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class LikeControllerTest extends AbstractControllerTest{
+
+    @Autowired
+    private LikeController likeController;
+
+    @Override
+    protected Object controller() {
+        return likeController;
+    }
+
+    @Test
+    public void addSongLike() throws Exception {
+
+        MultiValueMap<String, String> infos = new LinkedMultiValueMap<>();
+
+
+        mockMvc.perform(post("/like/addSongLike")).andExpect(status().isOk());
+    }
+}
