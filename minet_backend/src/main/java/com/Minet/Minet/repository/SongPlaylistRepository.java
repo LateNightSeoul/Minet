9573b457e1e5091cc71f803c02f16b354f1203b1
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-06-02 19:03:37 +0900

    refactor : SongPlaylist 기능 삭제

--- minet_backend/src/main/java/com/Minet/Minet/repository/SongPlaylistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Minet.Minet.repository;
-
-import com.Minet.Minet.domain.statistic.SongPlaylist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongPlaylistRepository extends JpaRepository<SongPlaylist, String> {
-}
