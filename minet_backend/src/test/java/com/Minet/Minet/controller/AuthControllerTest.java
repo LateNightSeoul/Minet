fc707b65822eca3527e5a8b7e39f1aca80842bf1
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-07-01 19:11:08 +0900

    test : test 객체 구현

---
+++ minet_backend/src/test/java/com/Minet/Minet/controller/AuthControllerTest.java
@@ -0,0 +1,16 @@
+package com.Minet.Minet.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void login() {
+    }
+}
