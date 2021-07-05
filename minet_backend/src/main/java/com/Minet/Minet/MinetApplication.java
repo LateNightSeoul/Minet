5981b8585b0ba71a9435582d70988e65c5e4f1d7
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-07-05 21:32:55 +0900

    Update MinetApplication.java

--- minet_backend/src/main/java/com/Minet/Minet/MinetApplication.java
+++ minet_backend/src/main/java/com/Minet/Minet/MinetApplication.java
@@ -17,13 +17,4 @@ public class MinetApplication {
 		SpringApplication.run(MinetApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
 }
