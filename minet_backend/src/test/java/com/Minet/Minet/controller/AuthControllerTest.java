ad1871e9529f2c50e4bef98ce9c5d34b05b09c6a
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-07-05 18:14:19 +0900

    test: 회원가입 테스트 구현

--- minet_backend/src/test/java/com/Minet/Minet/controller/AuthControllerTest.java
+++ minet_backend/src/test/java/com/Minet/Minet/controller/AuthControllerTest.java
@@ -1,9 +1,11 @@
 package com.Minet.Minet.controller;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import com.Minet.Minet.dto.member.JoinDto;
+import com.Minet.Minet.security.Authority;
+import com.Minet.Minet.security.WithMockJwtAuthentication;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AuthControllerTest {
 
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     public void setMockMvc(MockMvc mockMvc) {
@@ -29,21 +33,42 @@ class AuthControllerTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("회원 가입")
-    void join() {
+    void join() throws Exception {
+
+        JoinDto joinDto = JoinDto.builder()
+                .username("lee")
+                .email("lee@gamil.com")
+                .password("1234")
+                .phone("010-5612-2134")
+                .authority(Authority.ROLE_MEMBER)
+                .build();
+
+
+        ResultActions result = mockMvc.perform(
+                post("/join")
+                        .accept(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(joinDto))
+        );
+        result.andDo(print())
+                .andExpect(status().isOk());
+
     }
 
+
     @Test
+    @Order(2)
+    @WithMockJwtAuthentication
     @DisplayName("로그인 (토근이 올바른 경우)")
     void login() throws Exception {
         ResultActions result = mockMvc.perform(
                 post("/login")
-                        .accept(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
         );
         result.andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(handler().handlerType(AuthController.class))
-                .andExpect(handler().methodName("login"))
-                .andExpect(jsonPath("$."))
+                .andExpect(handler().methodName("login"));
+
     }
 }
